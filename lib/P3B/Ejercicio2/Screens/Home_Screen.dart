84380cc5172754e90c17fbbe88d99ffdd85ee9f7
paucortesyuste <paucortesyuste@gmail.com>
2025-03-12 14:10:46 +0100

    Ejercicio2

--- lib/P3B/Ejercicio2/Screens/Home_Screen.dart
+++ lib/P3B/Ejercicio2/Screens/Home_Screen.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
 
-class Homescreen extends StatefulWidget {
-  const Homescreen({super.key});
+import '../pages/page1.dart';
+import '../pages/page2.dart';
+import '../pages/page3.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<Homescreen> createState() => _HomescreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomescreenState extends State<Homescreen> {
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = <Widget>[
+    const page1(),
+    const page2(),
+    const page3(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Material App Bar"),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: _pages[_selectedIndex], // Mostrará la página correspondiente
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), // ✅ Icono corregido
+            label: 'page1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business), // ✅ Icono corregido
+            label: 'page2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school), // ✅ Icono corregido
+            label: 'page3',
+          ),
+        ],
       ),
     );
   }
